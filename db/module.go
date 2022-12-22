fce0d4cb2ce526c3d2031c5e493cb8ee39e42c62
AhmedNibras <nibrasahmad125@gmail.com>
2022-12-22 17:09:55 +0500

    fix:(database)

--- db/module.go
+++ db/module.go
@@ -8,13 +8,13 @@ import (
 )
 
 /*
-   DBConnection is supposed to connect to the sqliteDB and return a *sql.DB object.
+DBConnection is supposed to connect to the sqliteDB and return a *sql.DB object.
 
-   DBConnection is further used to fetch the data from database.
+DBConnection is further used to fetch the data from database.
 */
 func DBConnection() (*sql.DB, error) {
 	// connection to the database.
-	db, err := sql.Open("sqlite3", "./hawqalDB.sqlite")
+	db, err := sql.Open("sqlite3", "../hawqalData.sqlite")
 	if err != nil {
 		// handle the error.
 		return nil, fmt.Errorf("failed to connect database: %v", err)
