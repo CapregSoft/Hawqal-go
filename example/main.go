33eb0f2ec96a8278b76c2021e54019e6e06e4889
Husnain_Khurshid <muhammadhusnainkh@gmail.com>
2023-01-17 17:45:56 +0500

    fix: (hawqal-go consized)

--- example/main.go
+++ example/main.go
@@ -5,6 +5,7 @@ import (
 	"log"
 
 	hawqal "github.com/CapregSoft/Hawqal-go"
+	"github.com/CapregSoft/Hawqal-go/models"
 )
 
 func CheckError(err error) {
@@ -14,28 +15,28 @@ func CheckError(err error) {
 }
 func main() {
 
-	countries, err := hawqal.GetCountries()
-	CheckError(err)
-	fmt.Print(string(countries))
+	// countries, err := hawqal.GetCountries()
+	// CheckError(err)
+	// fmt.Print(string(countries))
 
-	country, err := hawqal.GetCountry("pakistan")
-	CheckError(err)
-	fmt.Print(string(country))
+	// country, err := hawqal.GetCountry("pakistan")
+	// CheckError(err)
+	// fmt.Print(string(country))
 
-	states, err := hawqal.GetStates()
-	CheckError(err)
-	fmt.Print(string(states))
+	// states, err := hawqal.GetStates()
+	// CheckError(err)
+	// fmt.Print(string(states))
 
-	state, err := hawqal.GetState("Punjab")
+	state, err := hawqal.GetState("Punjab", &models.StateFilter{CountryName: "pakistan", Filter: &models.StateData{StateName: true}})
 	CheckError(err)
 	fmt.Print(string(state))
 
-	cities, err := hawqal.GetCities()
-	CheckError(err)
-	fmt.Print(string(cities))
+	// cities, err := hawqal.GetCities()
+	// CheckError(err)
+	// fmt.Print(string(cities))
 
-	city, err := hawqal.GetCity("Lahore")
-	CheckError(err)
-	fmt.Print(string(city))
+	// city, err := hawqal.GetCity("Lahore")
+	// CheckError(err)
+	// fmt.Print(string(city))
 
 }
